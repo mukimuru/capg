14223ee7852d97eb8cb06035684a970e864f3723
mukimuru <mukimuru@DIN78000444.corp.capgemini.com>
2019-07-04 12:46:03 +0530

    Commited

---
+++ MyFirstproject/src/test/java/org/capg/demo/Mainclass.java
@@ -0,0 +1,14 @@
+package org.capg.demo;
+
+public class Mainclass {
+	public static void main(String[] args) {
+		System.out.println("welcome! sales Application!");
+		SalesClass obj=new SalesClass();
+		obj.show();
+		
+	}
+	public static void display() {
+		System.out.println("Hello! Good Morning!");
+	}
+
+}
---
+++ MyFirstproject/src/test/java/org/capg/demo/SalesClass.java
@@ -0,0 +1,11 @@
+package org.capg.demo;
+
+public class SalesClass {
+private int[] arr= {1,34,56,7};
+public void show() {
+	for(int num:arr) {
+		System.out.println(num);;
+		
+	}
+}
+}
